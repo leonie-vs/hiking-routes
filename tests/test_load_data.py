41956c7841fd5dd2e6c7a5c1086eb7fe68ebd127
Leonie von Schirach <leonie.schirach@web.de>
2026-02-19 17:08:09 +0000

    test: add test 2 to check get_all_routes method returns list of Hike objects

---
+++ tests/test_load_data.py
@@ -0,0 +1,16 @@
+import pytest
+from app.load_data import HikingRoutes
+from app.models import PublicTransportRoute, DurationHours, Hike
+
+# Test 1
+def test_HikingRoutes_file_path_can_only_be_str():
+    with pytest.raises(ValueError):
+        HikingRoutes(0)
+
+# Test 2
+def test_HikingRoutes_get_all_routes_method_returns_list_of_Hike_objects():
+    test = HikingRoutes("/Users/leonievs/Documents/coding-projects/hiking-routes/app/data/routes.json")
+    data = test.get_all_routes()
+    assert isinstance(data, list)
+  
+
